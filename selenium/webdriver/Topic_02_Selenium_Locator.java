defa7240e3b19e2d2abd5c70b2f27830badfcae6
Thang Nguyen <thang14.07.99@gmail.com>
2024-01-28 16:45:08 +0700

    Learn Data Type, Topic 02 - selenium locator

--- selenium/webdriver/Topic_02_Selenium_Locator.java
+++ selenium/webdriver/Topic_02_Selenium_Locator.java
@@ -1,5 +1,6 @@
 package webdriver;
 
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
 import org.testng.annotations.AfterClass;
@@ -23,23 +24,90 @@ public class   Topic_02_Selenium_Locator {
 
         driver = new FirefoxDriver();
         driver.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
+        driver.get("https://demo.nopcommerce.com/register");
         driver.manage().window().maximize();
     }
 
     @Test
-    public void TC_01() {
-
+    public void TC_01_ID() {
+        // Tìm element có id là FirstName
+        // System.out.println(driver.findElement(By.id("FirstName")));
+        driver.findElement(By.id("FirstName")).sendKeys("Thang Nguyen");
     }
 
     @Test
-    public void TC_02() {
-
+    public void TC_02_Class() {
+        driver.findElement(By.className("header-logo"));
     }
 
-//    @Test
-//    public void TC_03 {
-//
-//    }
+    @Test
+    public void TC_03_Name(){
+        driver.findElement(By.name("DateOfBirthDay"));
+    }
+
+    @Test
+    public void TC_04_Tagname(){
+        driver.findElement(By.tagName("input"));
+    }
+
+    @Test
+    public void TC_05_LinkedText(){
+        driver.findElement(By.linkText("Shipping & returns"));
+    }
+
+    @Test
+    public void TC_06_Partial_LinkedText(){
+        // Độ chính xác không cao. (Giống search kiểu link)
+        driver.findElement(By.partialLinkText("vendor account"));
+//        driver.findElement(By.partialLinkText("ount"));
+//        driver.findElement(By.partialLinkText("vendor"));
+    }
+
+    @Test
+    public void TC_07_Css(){
+        // Css với ID
+        driver.findElement(By.cssSelector("input[id='FirstName']"));
+        driver.findElement(By.cssSelector("input#FirstName"));
+        driver.findElement(By.cssSelector("#FirstName"));
+        // Css với Class
+        driver.findElement(By.cssSelector("div[class='page-title']"));
+        driver.findElement(By.cssSelector("div.page-title"));
+        driver.findElement(By.cssSelector(".page-title"));
+        // Css với tagname
+        driver.findElement(By.cssSelector("div"));
+        // Css với Link
+        driver.findElement(By.cssSelector("a[href='/customer/addresses']"));
+        // Css với partial Link
+        driver.findElement(By.cssSelector("a[href*='addresses']"));
+//        driver.findElement(By.cssSelector("div[href^='addresses']"));
+//        driver.findElement(By.cssSelector("div[href$='addresses']"));
+
+        // Css có thể tìm handle cả 6 loại trên
+    }
+
+    @Test
+    public void TC_08_XPath(){
+        // XPath với ID
+        driver.findElement(By.xpath("//input[@id='FirstName']"));
+        // XPath với Class
+        driver.findElement(By.xpath("//div[@class='page-title']"));
+        // XPath với tagname
+        driver.findElement(By.xpath("//div"));
+        // XPath với Link
+        driver.findElement(By.xpath("//a[@href='/customer/addresses']"));
+        driver.findElement(By.xpath("//a[text()='Addresses']"));
+        driver.findElement(By.xpath("//a[contains(text(),'Addresses')]"));
+        // XPath với partial Link
+        driver.findElement(By.xpath("//a[contains(@href,'addresses')]"));
+        driver.findElement(By.xpath("//a[contains(text(),'Addresses')]"));
+
+        // XPath có thể tìm handle tất cả các loại trên
+    }
+
+    @Test
+    public void TC_09_(){
+
+    }
 
     @AfterClass
     public void afterClass() {
